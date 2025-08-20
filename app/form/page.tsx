32f931129214ef86e0f7a8d4d8e126d7c8578a42
André <teamworkall247@gmail.com>
2025-08-20 15:27:04 +0200

    finished

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 import { FeedbackProvider } from "../context/FeedbackProvider"
+import Navigation from "../components/ui/Navigation"
+import { FeedbackItems } from "../utils/types"
 
 export default function Frompage() {
   return (
     <FeedbackProvider>
-      
+      <Navigation/>
+      <section className="p-10 flex flex-col gap-6">
+        <h1>Product FeedBack Form</h1>
+        <FormBody/>
+      </section>
     </FeedbackProvider>
   )
 }
